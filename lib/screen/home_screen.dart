4f77c861de27345823ad81c0a4ecac8f1ea872f1
김기혁 <gimgihyeog@gimgihyeog-ui-MacBookPro.local>
2024-06-26 13:13:06 +0900

    First Commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        bottom: false,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              _Top(
+                selectedDate: selectedDate,
+                onPressed: onHeartButtonPressed,
+              ),
+              _Bottom(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  onHeartButtonPressed() {
+    showCupertinoDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.center,
+          child: Container(
+            color: Colors.white,
+            height: 300,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: selectedDate,
+              maximumDate: DateTime.now(),
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  selectedDate = date;
+                });
+              },
+              dateOrder: DatePickerDateOrder.ymd,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _Top extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final DateTime selectedDate;
+  const _Top({
+    required this.selectedDate,
+    required this.onPressed,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+    final textTheme = Theme.of(context).textTheme;
+
+    return Expanded(
+      child: Container(
+        child: Column(
+          children: [
+            Text(
+              'Kim&???',
+              style: textTheme.displayLarge,
+            ),
+            Text(
+              '우리 처음 만난 날',
+              style: textTheme.bodyLarge,
+            ),
+            Text(
+              '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+              style: textTheme.bodyMedium,
+            ),
+            IconButton(
+              iconSize: 60,
+              color: Colors.red,
+              onPressed: onPressed,
+              icon: Icon(Icons.favorite),
+            ),
+            Text(
+              'D+${now.difference(selectedDate).inDays + 1}',
+              style: textTheme.displayMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Bottom extends StatelessWidget {
+  const _Bottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Image.asset(
+          'asset/img/middle_image.png',
+        ),
+      ),
+    );
+  }
+}
